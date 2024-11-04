7ece02e88375b433f9e8a52dbe862fa7ab699666
fvesanen <fanni.vesanen@gmail.com>
2024-11-04 16:30:58 +0200

    clean input and post removed charachters

--- ascii-art-web/ascii/ascii.go
+++ ascii-art-web/ascii/ascii.go
@@ -9,45 +9,45 @@ import (
 // reading the banner file and printing ascii art matching the input argument
 func PrintAsciiArt(input, banner string) string {
 
-	var result string
-	input = cleanInput(input)
+	var result strings.Builder
+
 	if len(input) == 0 {
 		return "Error"
 	}
 	bannerFile, err := os.ReadFile("./banners/" + banner + ".txt")
 	if err != nil {
 		log.Print("ERROR: Couldn't read the banner file: ", err)
-		result = "ERROR: banner issues"
+		result.WriteString("ERROR: banner issues")
 	} else if len(input) != 0 {
 		cleanBannerFile := cleanBanner(bannerFile)
 		bannerFileLines := strings.Split(string(cleanBannerFile), "\n")
 
 		input = strings.ReplaceAll(input, "\\n", "\n")
 		inputSlice := strings.Split(input, "\n")
+		removedChar := ""
+		inputSlice, removedChar = cleanInput(inputSlice)
 
-		onlyNewLines := true
+		if removedChar != "" {
+			result.WriteString("These characters were removed: " + removedChar + "\n\n")
+		}
 
 		//An empty index generates a newline, else the index is looped to match indexes from bannerFileLines
 		for _, inputLine := range inputSlice {
 			if inputLine == "" {
-				result += "\n"
+				result.WriteString("\n")
 			} else {
-				onlyNewLines = false
 				for i := 1; i <= 8; i++ {
 					for _, char := range inputLine {
-						result += bannerFileLines[i+(int(char-32)*9)]
+						result.WriteString(bannerFileLines[i+(int(char-32)*9)])
 					}
-					result += "\n"
+					result.WriteString("\n")
 				}
 			}
 		}
-		//if the input consists only newlines, deducting one newline
-		if onlyNewLines {
-			result = result[1:]
-		}
+
 	}
 
-	return result
+	return result.String()
 }
 
 // Replacing carriage return with a newline
@@ -58,15 +58,35 @@ func cleanBanner(fileContent []byte) string {
 }
 
 // Removing everything that is not printable ascii character nor new line from the input
-func cleanInput(s string) string {
-	cleanInput := ""
+func cleanInput(input []string) ([]string, string) {
+	onlyNewLines := true
+	var cleanInput []string
+	var removedChar strings.Builder
 
-	for _, char := range s {
-		if char >= 32 && char <= 127 || char == '\n' {
-			cleanInput += string(char)
+	for _, lines := range input {
+		if lines == "" {
+			cleanInput = append(cleanInput, lines)
+		} else {
+			var printableChars strings.Builder
+			onlyNewLines = false
+			for _, char := range lines {
+				if char >= 32 && char <= 126 {
+					printableChars.WriteString(string(char))
+				} else {
+					removedChar.WriteString(string(char) + " ")
+				}
+			}
+			if printableChars.String() != "" {
+				cleanInput = append(cleanInput, printableChars.String())
+			}
 		}
+
 	}
-	return cleanInput
+	if onlyNewLines {
+		cleanInput = cleanInput[:1]
+	}
+
+	return cleanInput, removedChar.String()
 }
 
 // lisää infoboxi jossa kerrotaan käyttäjälle mitkä charachterit toimii inputkentässä
