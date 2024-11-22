9d1faa5c479afe5c10db0aaf5735d529f565accb
Roope <roope.hongisto@gritlab.ax>
2024-11-22 11:19:49 +0200

    made shell script to start the container

--- ascii-art-web/build.sh
+++ ascii-art-web/build.sh
@@ -7,3 +7,7 @@ TAG="latest"
 # Build the docker image
 echo "Building Docker image: $IMAGE_NAME:$TAG..."
 docker build -t $IMAGE_NAME:$TAG .
+
+# Start container on port 8080
+echo "Starting container: $IMAGE_NAME:$TAG..."
+docker run -p 8080:8080 --name asciiweb $IMAGE_NAME
